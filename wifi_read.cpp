66cdc84c9439509de053ef11aa13c559f6a040d8
johnny <104272459+JohnnyNg6@users.noreply.github.com>
2025-03-07 23:45:09 +0800

    Create wifi_read.cpp

---
+++ wifi_read.cpp
@@ -0,0 +1,71 @@
+#include "wifi_read.h"
+
+
+
+void WiFi_Read() {
+
+  int statusCode = 0;
+  
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass); // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected");
+  }
+
+  // Read in field 4 of the public channel recording the temperature
+  float temperatureInF = ThingSpeak.readFloatField(weatherStationChannelNumber, temperatureFieldNumber);  
+
+  // Check the status of the read operation to see if it was successful
+  statusCode = ThingSpeak.getLastReadStatus();
+  if(statusCode == 200){
+    Serial.println("Temperature at MathWorks HQ: " + String(temperatureInF) + " deg F");
+  }
+  else{
+    Serial.println("Problem reading channel. HTTP error code " + String(statusCode)); 
+  }
+  
+  delay(15000); // No need to read the temperature too often.
+
+  // Read in field 1 of the private channel which is a counter  
+  long count = ThingSpeak.readLongField(counterChannelNumber, counterFieldNumber, myCounterReadAPIKey);  
+
+   // Check the status of the read operation to see if it was successful
+  statusCode = ThingSpeak.getLastReadStatus();
+  if(statusCode == 200){
+    Serial.println("Counter: " + String(count));
+  }
+  else{
+    Serial.println("Problem reading channel. HTTP error code " + String(statusCode)); 
+  }
+  
+  delay(15000); // No need to read the counter too often.
+  
+}
+
+
+void setEspBaudRate(unsigned long baudrate){
+  long rates[6] = {115200,74880,57600,38400,19200,9600};
+
+  Serial.print("Setting ESP8266 baudrate to ");
+  Serial.print(baudrate);
+  Serial.println("...");
+
+  for(int i = 0; i < 6; i++){
+    Serial1.begin(rates[i]);
+    delay(100);
+    Serial1.print("AT+UART_DEF=");
+    Serial1.print(baudrate);
+    Serial1.print(",8,1,0,0\r\n");
+    delay(100);  
+  }
+    
+  Serial1.begin(baudrate);
+}
+
+
+
